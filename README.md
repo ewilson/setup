4d567776a4ecc06367f13b9cb1482c3d2fce99df
Eric Wilson <wilson.eric.n@gmail.com>
2013-10-04 08:35:45 -0400

    Added comments, renamed file

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ To get started on a fresh Ubuntu box:
 
     sudo apt-get install git
     git clone https://github.com/ewilson/setup.git
-    sudo setup/setup.sh
+    sudo setup/basic.sh
 
 For documentation, see the script itself.
--- basic.sh
+++ basic.sh
@@ -3,14 +3,19 @@
 # This is a minimal setup script for an Ubuntu Box for Python development
 # for students. 
 
-echo "UPDATING AND UPGRADING PACKAGES"
 set -x
+
+echo "UPDATING AND UPGRADING PACKAGES"
+# Always necessary after installation
 apt-get install update
 apt-get install upgrade
 
+# Things I want on any machine I use
 echo "INSTALLING DEV UTILITIES"
 apt-get install gitg tree tmux ack-grep ipython emacs curl
 
+# pip is essential for Python development. 
+# Installing flask verifies that pip is working.
 echo "INSTALLING PIP & FLASK"
 curl -O http://python-distribute.org/distribute_setup.py
 python distribute_setup.py
@@ -18,16 +23,19 @@ curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
 python get-pip.py
 pip install flask
 
+# Sorry Firefox, I need Chrome.
 echo "INSTALLING CHROME"
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update 
 sudo apt-get install google-chome-stable
 
+# This is for the 'right-click to open terminal'
 echo "INSTALLING NAUTILUS"
 sudo apt-get install nautilus-open-terminal
 nautilus -q
 
+# So I don't find myself in nano, ever.
 echo "SETTING EDITOR TO EMACS"
 echo "export EDITOR='emacs -nw'" | sudo tee -a /etc/profile
 source /etc/profile
