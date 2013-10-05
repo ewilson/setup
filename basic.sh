d39419e2d3ef80cdd57cabb856364e6a52aecb43
Eric Wilson <wilson.eric.n@gmail.com>
2013-10-04 20:00:40 -0400

    possible fix for chrome

--- basic.sh
+++ basic.sh
@@ -25,10 +25,9 @@ pip install flask
 
 # Sorry Firefox, I need Chrome.
 echo "INSTALLING CHROME"
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update 
-sudo apt-get install google-chome-stable
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i ./google-chrome*.deb
+apt-get install -f
 
 # This is for the 'right-click to open terminal'
 echo "INSTALLING NAUTILUS"
