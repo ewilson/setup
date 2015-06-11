da8f234de286768cb23c89fd33e189b1b70af2bb
Eric Wilson <wilson.eric.n@gmail.com>
2015-06-11 13:02:49 -0400

    Update server.sh

--- server.sh
+++ server.sh
@@ -2,7 +2,9 @@
 #
 # This is a setup script for an Ubuntu server for Python deployment
 #
-
+# to create ec2 instance:
+# aws ec2 run-instances --image-id ami-d05e75b8 --count 1 --instance-type t2.micro --key-name tm-api --security-groups python-web-server --profile eric
+#
 set -x
 
 echo "UPDATING AND UPGRADING PACKAGES"
@@ -12,7 +14,7 @@ apt-get upgrade
 
 # Things I want on any machine I use
 echo "INSTALLING DEV UTILITIES"
-apt-get install tree tmux ack-grep ipython emacs24 curl python-pip sqlite3
+apt-get install tree tmux ack-grep ipython emacs curl sqlite3 python3-pip python-pip
 ln -s /usr/bin/ack-grep /usr/local/bin/ack 
 pip install -U ipython pytest virtualenv
 
@@ -38,5 +40,21 @@ git config --global github.user 'ewilson'
 git config --global push.default 'simple'
 git config --global color.ui 'true'
 
+echo "setting up virtualenvwrapper"
+sudo pip install virtualenvwrapper
 
+#export WORKON_HOME=$HOME/.virtualenvs
+#export PROJECT_HOME=$HOME
+#source /usr/local/bin/virtualenvwrapper.sh
+#export SECRET_KEY='83f900611c31ac8551e38d04d3f457b36f50c155'
+
+git clone git://github.com/ewilson/titlematch_api.git
+cd titlematch_api
+mkvirtualenv tma
+workon tma
+pip3 install -r requirements.txt
+
+# in virtualenv
+pip3 install gunicorn
+gunicorn titlematch_api.wsgi
 
